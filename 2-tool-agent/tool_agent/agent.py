80a38731aa9d25875cffadfe20bcda92c47fbfed
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-05-12 02:35:34 -0700

    added

--- 2-tool-agent/tool_agent/agent.py
+++ 2-tool-agent/tool_agent/agent.py
@@ -1,14 +1,15 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+from datetime import datetime
 
-# def get_current_time() -> dict:
-#     """
-#     Get the current time in the format YYYY-MM-DD HH:MM:SS
-#     """
-#     return {
-#         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-#     }
-
+def get_current_time() -> dict:
+    """
+    Get the current time in the format YYYY-MM-DD HH:MM:SS
+    """
+    return {
+        "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+# note for an agent you can only pass in, one built in tool at a time
 root_agent = Agent(
     name="tool_agent",
     model="gemini-2.0-flash",
@@ -17,6 +18,7 @@ root_agent = Agent(
     You are a helpful assistant that can use the following tools:
     - google_search
     """,
+    # only one built in tool at a time
     tools=[google_search],
     # tools=[get_current_time],
     # tools=[google_search, get_current_time], # <--- Doesn't work
